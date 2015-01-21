9f190474f4930fe2a599fc4bf792d5953a503700
William <william781201@gmail.com>
2015-01-21 05:05:59 -0800

    a lot changes Signed-off-by:William <william781201@gmail.com>

---
+++ application/controllers/About.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of About
+ *
+ * @author william
+ */
+class About {
+    public function index()
+	{
+		$this->load->view('about');
+	}
+}
